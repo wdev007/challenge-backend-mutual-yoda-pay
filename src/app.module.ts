1f9bad93f397cd3bce580697b78c7def60486414
Wellici Araujo <wja1@ifal.edu.br>
2021-11-06 02:48:39 -0300

    created account module and docker configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountsModule } from './accounts/accounts.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { databaseFactory } from './config/database';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env' })],
+      inject: [ConfigService],
+      useFactory: databaseFactory,
+    }),
+    AccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
